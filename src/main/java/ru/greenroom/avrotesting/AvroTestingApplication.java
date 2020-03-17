77b7d3296ec12a90e35c3b116f0a3c2912a3e48d
Nariman Abdullin <nulls.narik@gmail.com>
2020-03-17 23:09:30 +0300

    init project

---
+++ src/main/java/ru/greenroom/avrotesting/AvroTestingApplication.java
@@ -0,0 +1,13 @@
+package ru.greenroom.avrotesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/greenroom/avrotesting/AvroTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.greenroom.avrotesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
